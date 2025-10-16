f76d093b00433ade685aef57ce9c27842699c6c5
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-10-16 19:51:21 +0500

    chore: tested

---
+++ tests/test-googledrive.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/bootstrap.php';
+
+class WPMUDEV_GoogleDrive_Test extends TestCase {
+
+    public function test_gdrive_token_option() {
+        update_option('wpmudev_gdrive_token', 'dummy-token');
+        $token = get_option('wpmudev_gdrive_token');
+        $this->assertEquals('dummy-token', $token);
+        delete_option('wpmudev_gdrive_token');
+    }
+
+    public function test_admin_page_registered() {
+        global $menu;
+        $page_found = false;
+        foreach ($menu as $menu_item) {
+            if (isset($menu_item[2]) && $menu_item[2] === 'wpmudev-plugin-test-googledrive') {
+                $page_found = true;
+                break;
+            }
+        }
+        $this->assertTrue($page_found);
+    }
+}
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -11,6 +11,7 @@ return array(
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
     'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'Psr\\Cache\\' => array($vendorDir . '/psr/cache/src'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'ParagonIE\\ConstantTime\\' => array($vendorDir . '/paragonie/constant_time_encoding/src'),
     'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
@@ -21,4 +22,6 @@ return array(
     'Google\\Auth\\' => array($vendorDir . '/google/auth/src'),
     'Google\\' => array($vendorDir . '/google/apiclient/src'),
     'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );

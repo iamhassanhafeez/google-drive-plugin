ecc11d21f9e1df364122fd38e5b85c2e0520e107
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-10-17 00:55:00 +0500

    chore: reoslved some build issues

--- assets/js/drivetestpage.min.asset.php
+++ assets/js/drivetestpage.min.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'wp-components', 'wp-element' ),
-	'version'      => '5125299c8d6b59d937a2',
-);
+<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => '5125299c8d6b59d937a2');
